e60fdffa3eab911bc1b761a4aa423dbb3a5b5b13
Christian Foellmann <foellmann@foe-services.de>
2015-03-27 22:03:52 +0100

    std config - php generated output code styling

--- Library/Configuration/Loader.php
+++ Library/Configuration/Loader.php
@@ -45,26 +45,29 @@ class Library_Configuration_Loader
                                        'file_path',
                                        'servers');
 
-    protected static $_iniStdArray = array(
-        'stats_api'          => 'Server',
-        'slabs_api'          => 'Server',
-        'items_api'          => 'Server',
-        'get_api'            => 'Server',
-        'set_api'            => 'Server',
-        'delete_api'         => 'Server',
-        'flush_all_api'      => 'Server',
+    protected static $_iniStdArray = array (
+        'stats_api' => 'Server',
+        'slabs_api' => 'Server',
+        'items_api' => 'Server',
+        'get_api' => 'Server',
+        'set_api' => 'Server',
+        'delete_api' => 'Server',
+        'flush_all_api' => 'Server',
         'connection_timeout' => '1',
-        'max_item_dump'      => '100',
-        'refresh_rate'       => 2,
-        'memory_alert'       => '80',
-        'hit_rate_alert'     => '90',
-        'eviction_alert'     => '0',
-        'file_path'          => 'Temp/',
-        'servers'            => array (
-            'Default' => array (
-                '127.0.0.1:11211' => array (
+        'max_item_dump' => '100',
+        'refresh_rate' => 2,
+        'memory_alert' => '80',
+        'hit_rate_alert' => '90',
+        'eviction_alert' => '0',
+        'file_path' => 'Temp/',
+        'servers' =>
+        array (
+            'Default' =>
+            array (
+                '127.0.0.1:11211' =>
+                array (
                     'hostname' => '127.0.0.1',
-                    'port'     => '11211',
+                    'port' => '11211',
                 ),
             ),
         ),
