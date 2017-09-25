06841ee19a7f4ce6f5e231c52f7a26c635320e59
Christian Foellmann <foellmann@foe-services.de>
2017-09-25 12:21:21 +0200

    removed __autoloader function

--- Library/Loader.php
+++ Library/Loader.php
@@ -1,15 +1,13 @@
 <?php
 # Constants declaration
-define('CURRENT_VERSION', '1.3.0');
+define('CURRENT_VERSION', '1.2.3');
 
 # PHP < 5.3 Compatibility
-if(!defined('ENT_IGNORE'))
-{
+if ( ! defined('ENT_IGNORE')) {
     define('ENT_IGNORE', 0);
 }
 
 # Autoloader
-function __autoload($class)
-{
+spl_autoload_register(function ($class) {
     require_once str_replace('_', DIRECTORY_SEPARATOR, $class) . '.php';
-}
+});
